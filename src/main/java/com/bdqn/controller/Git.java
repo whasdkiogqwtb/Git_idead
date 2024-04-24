ca9c599908470135092be3948da881172e906306
Yhy <1643159947@qq.com>
2024-04-24 16:41:39 +0800

    hot-fix提交

--- src/main/java/com/bdqn/controller/Git.java
+++ src/main/java/com/bdqn/controller/Git.java
@@ -3,17 +3,7 @@ package com.bdqn.controller;
 public class Git {
     public void test(){
         System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("master test");
-        System.out.println("添加");
-        System.out.println("master");
-        System.out.println("push test");
-        System.out.println("pull test");
-        System.out.println("pull test2");
-        System.out.println("pull test3");
-        System.out.println("pull test4");
+
 
     }
 }
