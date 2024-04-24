33106764b2b27261f0b3e8a6db93cbc0aa29e35e
Yhy <1643159947@qq.com>
2024-04-24 15:55:24 +0800

    修改

--- src/main/java/com/bdqn/controller/Git.java
+++ src/main/java/com/bdqn/controller/Git.java
@@ -4,5 +4,9 @@ public class Git {
     public void test(){
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("master test");
+        System.out.println("添加");
     }
 }
